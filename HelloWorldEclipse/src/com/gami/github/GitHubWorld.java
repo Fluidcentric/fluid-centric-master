d85dd3c35f58730bc7fb195ee1305ed47e36f751
Hgami19 <hgami07@gmail.com>
2018-05-08 23:33:52 -0600

    Just a master!

--- HelloWorldEclipse/src/com/gami/github/GitHubWorld.java
+++ HelloWorldEclipse/src/com/gami/github/GitHubWorld.java
@@ -3,6 +3,5 @@ package com.gami.github;
 public class GitHubWorld {
 	public static void main(String[] args) {
 		System.out.println("Welcome to GitHub World -> Gami! ");
-		System.out.println("Trying to commit to child -> Then to Master !");
 	}
 }
