5ae3147aec7d9d4ba558192e9e0b4a13d0ce53e8
Hgami19 <hgami07@gmail.com>
2018-05-08 22:12:35 -0600

    Demo Commit

--- HelloWorldEclipse/src/com/gami/github/GitHubWorld.java
+++ HelloWorldEclipse/src/com/gami/github/GitHubWorld.java
@@ -4,7 +4,7 @@ public class GitHubWorld {
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to GitHub World - Gami! ");
+		System.out.println("Welcome to GitHub World -> Gami! ");
 	}
 
 }
