a33afaa2885b4dab3ce9b4c5e339224e820ac3ad
Hgami19 <hgami07@gmail.com>
2018-06-25 21:59:32 -0600

    Trying secound commit

--- HelloWorldEclipse/src/com/gami/github/GitHubWorld.java
+++ HelloWorldEclipse/src/com/gami/github/GitHubWorld.java
@@ -2,6 +2,7 @@ package com.gami.github;
 
 public class GitHubWorld {
 	public static void main(String[] args) {
-		System.out.println("Welcome to GitHub World -> Gami! ");
+		System.out.println("Welcome to GitHub World -> Gami!");
+		System.out.println("Making secound commit! ");
 	}
 }
